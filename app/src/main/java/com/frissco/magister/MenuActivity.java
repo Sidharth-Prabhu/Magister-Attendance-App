4f8149c5edf80cf8ed70e2476ef6fd230c115987
Cyber-Zypher <cyberzypher@gmail.com>
2024-01-15 17:21:38 +0530

    Initial Commit

--- app/src/main/java/com/frissco/magister/MenuActivity.java
+++ app/src/main/java/com/frissco/magister/MenuActivity.java
@@ -3,21 +3,34 @@ package com.frissco.magister;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class MenuActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    LinearLayout getSourceCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         setToolBar();
+        getSourceCode = findViewById(R.id.getSourceCode);
+        getSourceCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/Cyber-Zypher/Magister-Attendance-App";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 
     private void setToolBar() {
